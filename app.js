637865f4d57b1f9e26e430aa894a29da7c4930d9
akghosh111 <ghoshkiran2017@gmail.com>
2024-07-14 18:46:14 +0530

    setup a http server & added socket.io

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const express = require('express')
+const express = require("express")
 const app = express()
+const http = require("http");
 const port = 3000
+
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+
+const io = socketio(server);
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.listen(port)
+server.listen(port);
