18b79edbc9f2cf9a24b2809238401dca4265a9a3
akghosh111 <ghoshkiran2017@gmail.com>
2024-07-14 19:07:15 +0530

    added custom & leaflet css, js & setup ejs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const http = require("http");
+const path = require("path");
 const port = 3000
 
 const socketio = require("socket.io");
@@ -8,6 +9,9 @@ const socketio = require("socket.io");
 const server = http.createServer(app);
 
 const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
 
