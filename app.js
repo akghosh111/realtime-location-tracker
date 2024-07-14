f08317727a575da7abf6f4ee0a19daff6c8415d9
akghosh111 <ghoshkiran2017@gmail.com>
2024-07-14 20:50:04 +0530

    sent location data - long, lat to front-end

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.use(express.json());
 
 io.on("connection", function(socket){
+    socket.on("send-location", function (data){
+        io.emit("receive-location", {id: socket.id, ...data});
+    })
     console.log("connected");
 
 });
--- public/js/script.js
+++ public/js/script.js
@@ -19,3 +19,12 @@ if(navigator.geolocation){
 const map = L.map("map").setView([0,0], 10);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+
+const markers = {};
+
+socket.on("receive-location", (data)=>{
+    const {id, latitude, longitude} = data;
+    map.setView([latitude, longitude]);
+})
+
